325c7894c3941d5d7046cc7d751104f33dafa938
jbondu9 <jimmy.bondu@zoho.com>
2021-02-26 20:47:11 +0100

    add content and footer to the form

--- components/content/ShortPresentation.vue
+++ components/content/ShortPresentation.vue
@@ -8,8 +8,8 @@
           <h1 class="name">La candidatue spontanée</h1>
           <ul class="contact-info">
             <li>Identité : Jimmy Bondu</li>
-            <li>Poste visé : développeur front-end</li>
-            <li>Niveau : Junior mais motivé</li>
+            <li>Poste visé : Développeur front-end</li>
+            <li>Niveau : Junior</li>
           </ul>
         </div>
         <div class="quick-availability">
@@ -30,35 +30,49 @@
       <div class="form-content">
         <div class="contact-mode">
           <div class="media">
-            <span class="label">Contacter via</span>
+            <span class="label">Contacter par</span>
             :
             <span>{{ media }}</span>
           </div>
           <div class="media-list">
-            <select v-model="media">
-              <option>Mail</option>
-              <option>LinkedIn</option>
-              <option v-if="mobile">Téléphone</option>
-            </select>
+            <div class="media-option mail" role="button" @click="setMedia('Mail')" :class="{active: media === 'Mail'}"></div>
+            <div class="media-option linkedin" role="button" @click="setMedia('LinkedIn')" :class="{active: media === 'LinkedIn'}"></div>
+            <div v-if="mobile" class="media-option phone" role="button" @click="setMedia('Téléphone')" :class="{active: media === 'Téléphone'}"></div>
           </div>
           <div class="mail-object" v-if="media === 'Mail'">
             <div class="object">
-              <span class="label">Objet</span>
-              :
-              <span>{{ object }}</span>
+              <div>
+                <span class="label">Objet</span>
+                :
+                <span>{{ object }}</span>
+              </div>
+              <div>
+                <span class="guide">Guide des objets</span>
+              </div>
             </div>
             <div class="object-list">
-              <select v-model="object">
-                <option>S</option>
-                <option>M</option>
-                <option>L</option>
-              </select>
+              <div class="object-option" role="button" @click="setObject('S')" :class="{active: object === 'S'}">
+                <span>S</span>
+              </div>
+              <div class="object-option" role="button" @click="setObject('M')" :class="{active: object === 'M'}">
+                <span>M</span>
+              </div>
+              <div class="object-option" role="button" @click="setObject('L')" :class="{active: object === 'L'}">
+                <span>L</span>
+              </div>
             </div>
           </div>
         </div>
-        <button type="submit">
+        <button type="submit" class="form-btn">
           <span>Contacter</span>
         </button>
+      </div>
+      <div class="form-footer">
+        <div class="shipping">Prise de poste estimée : À partir du 29 mars</div>
+        <div class="remaining-days">
+          <div class="days">30 jours</div>
+          <div class="info">restant avant d'être disponible</div>
+        </div>
       </div>
     </form>
   </section>
@@ -86,13 +100,23 @@
       else {
         this.mobile = false
       }
+    },
+
+    methods: {
+      setMedia(media) {
+        this.media = media
+      },
+
+      setObject(object) {
+        this.object = object
+      }
     }
   }
 </script>
 
 <style scoped>
   .contact-form {
-    padding: 16px 24px 0 24px;
+    padding: 16px 24px 30px 24px;
   }
 
   .form-header {
@@ -171,14 +195,139 @@
     margin-bottom: 10px;
   }
 
+  .media-list, .object-list {
+    display: flex;
+  }
+
+  .media-list {
+    margin-bottom: 24px;
+  }
+
+  .media-option {
+    position: relative;
+    width: 42px;
+    height: 42px;
+    margin-right: 10px;
+    border-radius: 50%;
+    border: 2px solid #e3e3e3;
+    cursor: pointer;
+    transition: all .25s;
+  }
+
+  .media-option.active {
+    border: 2px solid #000;
+  }
+
+  .media-option.mail {
+    background: center / 60% no-repeat url("~assets/img/mail.svg"),
+                linear-gradient(rgb(0, 102, 255), rgb(0, 200, 255));
+  }
+
+  .media-option.linkedin {
+    background: center / 90% no-repeat url("~assets/img/linkedin.svg");
+  }
+
+  .media-option.phone {
+    background: center / 60% no-repeat url("~assets/img/call.svg"),
+                linear-gradient(rgb(150, 255, 79), rgb(48, 212, 15));
+  }
+
+  .media-option::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: calc(100% - 4px);
+    height: calc(100% - 4px);
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    border: 2px solid #fff;
+  }
+
+  .object {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+  }
+
   .label {
     font-family: "Qanelas-Bold";
     font-weight: 700;
   }
 
-  .media-list {
+  .guide {
+    text-decoration: underline;
+  }
+
+  .object-list {
+    margin-bottom: 16px;
+  }
+
+  .object-option {
+    position: relative;
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+    border: 2px solid #e3e3e3;
+    cursor: pointer;
+    transition: all .25s;
+  }
+
+  .object-option.active {
+    font-family: "Qanelas-Bold";
+    font-weight: 700;
+    border: 2px solid #000;
+  }
+
+  .object-option > span {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .form-btn {
+    width: 100%;
+    margin: 12px 0 10px 0;
+    padding: 12px 0;
+    font-size: 18px;
+    font-family: "Qanelas-Bold";
+    font-weight: 700;
+    color: #fff;
+    background-color: #070e15;
+    border: none;
+    cursor: pointer;
+  }
+
+  .form-footer {
     display: flex;
+    flex-flow: column;
+    align-items: center;
+    font-family: "Qanelas";
+  }
+
+  .shipping {
     margin-bottom: 24px;
+    line-height: 1.2;
+    text-align: center;
+  }
+
+  .days {
+    margin-bottom: 8px;
+    color: #192631;
+    font-family: "Qanelas-Bold";
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 100%;
+    text-align: center;
+    text-shadow: 0 2px 15px rgba(0,0,0,.1);
+  }
+
+  .info {
+    color: #192631;
+    text-align: center;
+    text-shadow: 0 2px 15px rgba(0,0,0,.1);
   }
 
   @media only screen and (max-width: 767px) {
