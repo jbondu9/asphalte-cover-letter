b0b694f8d70cc8003476dcc04abf01e574ba38c4
jbondu9 <jimmy.bondu@zoho.com>
2021-03-19 20:25:03 +0100

    Updates

--- _nuxt/static/1616181387/available/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fasphalte-cover-letter\u002F_nuxt\u002Fstatic\u002F1616181387",layout:"default",error:null,state:{modal:{toggleModal:a},nav:{toggleDrawer:a}},serverRendered:true,routePath:"\u002Favailable",config:{}}}(false));
---
+++ _nuxt/static/1616181828/available/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fasphalte-cover-letter\u002F_nuxt\u002Fstatic\u002F1616181828",layout:"default",error:null,state:{modal:{toggleModal:a},nav:{toggleDrawer:a}},serverRendered:true,routePath:"\u002Favailable",config:{}}}(false));
