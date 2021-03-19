804c9c7645ab1056026be54dd596d362cee8fae0
jbondu9 <jimmy.bondu@zoho.com>
2021-03-19 20:18:12 +0100

    Updates

--- _nuxt/static/1616142307/experiences/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fasphalte-cover-letter\u002F_nuxt\u002Fstatic\u002F1616142307",layout:"default",error:null,state:{modal:{toggleModal:a},nav:{toggleDrawer:a}},serverRendered:true,routePath:"\u002Fexperiences",config:{}}}(false));
